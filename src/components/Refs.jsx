5601e1a8a99f108c26d18d74fa5905efba8a1615
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-06-02 17:36:19 -0300

    Usando las refs

--- src/components/Refs.jsx
+++ src/components/Refs.jsx
@@ -1,15 +1,20 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment, createRef } from 'react';
 
 class Refs extends Component {
-    
+
+    constructor(){
+        super()
+        this.inputText = createRef()
+    }
+
     handleFocus = () => {
-        document.getElementById('input').focus()
+        this.inputText.current.focus()
     }
 
     render() {
         return (
             <Fragment>
-                <input type="text" id="input"/>        
+                <input type="text" ref={this.inputText} />        
                 <button onClick={this.handleFocus}>Focus</button>
             </Fragment>
         );
@@ -20,31 +25,27 @@ export default Refs;
 
 
 /*
-¿Por qué usar refs?
+Usando las refs
 
-Existen dos formas de controlar formularios en React.
+Para crear la referencia podemos usar 'this.inputText = React.createRef()'
+o
+destructurarlo (pasar 'createRef') en el import y luego usar 'this.inputText = createRef()'
 
-A traves de los input controlados y a traves de los input no controlados.
+Es preferible destructurar para tener un codigo mas limpio.
 
-Los inputs controlados se gestionan con el estado.
-Los inputs no controlados se gestionan con las refs.
 
-Podemos seleccionar el elemento y hacer foco sobre el usando 
+No es obligatorio crear la referencia con un constructor.
 
-handleFocus = () => {
-    document.getElementById('input').focus()
+Si tenemos el metodo constructor debemos crear la referencia dentro de este:
+
+constructor(){
+    super()
+    this.inputText = createRef()
 }
 
-pero esta manera presenta algunos inconvenientes por que 'document.getElementById' es un meotodo del DOM y React no trabaja sobre el DOM.
+pero si no tenemos el metodo constructor podemos crear la referencia de la siguiente manera:
 
-React trabaja sobre el virtual DOM.
-
-Si este elemento lo desmontamos y necesitamos acceder a el de alguna forma, este metodo 'document.getElementById' ya no va a funcionar porque no existe ningun elemento con el id que estamos buscando.
-
-Por lo tanto React creo algo que se denominan Refs (referencias).
-
-Las refs son el equivalente a 'document.getElementById' pero en el virtual DOM.
-
+inputText = createRef()
 
 
 
